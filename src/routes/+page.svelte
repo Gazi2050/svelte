3f9fd2d19cbbf7594478f2c27047bc183c910702
Gazi2050 <gazi45344@gmail.com>
2024-11-06 20:25:00 +0600

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 <main>
     <p class="text-2xl font-bold m-5">Step:{formState.step}</p>
     {#if formState.step === 0}
-        <div>
+        <div class="m-2">
             <label for="name">Name :</label>
             <input
                 class="border"
@@ -18,6 +18,23 @@
                 id="name"
                 bind:value={formState.name}
             />
+            <button
+                class="border p-1 bg-orange-200 rounded"
+                onclick={() => {
+                    if (formState.name !== "") {
+                        formState.step += 1;
+                        formState.error = "";
+                    } else {
+                        formState.error = "Enter Name";
+                    }
+                }}>Next</button
+            >
         </div>
+    {/if}
+
+    {#if formState.error}
+        <p class="text-xl font-bold text-red-600 m-2">
+            {formState.error}
+        </p>
     {/if}
 </main>
